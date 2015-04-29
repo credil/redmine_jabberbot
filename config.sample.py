6914ae423972a04a25c7808bebfdca051852878a
Julien Lamarche <jlam@credil.org>
2015-04-29 14:27:08 -0400

    updated sample config

--- config.sample.py
+++ config.sample.py
@@ -6,4 +6,5 @@ xmppHandles = {'redmineUser1': 'xmppUser1', 'redmineUser2':'xmppUser2'}
 ignoreUsers = ('imGone', 'iquit', 'iDissapeared')
 userConfig = {'user1': {'threshold':0.5}, 'user2':{'threshold':8}}
 conn_string = "host='localhost' dbname='redmine' user='blibli' password='blablabla'"
+firstNames = {'jlam': 'Julien', 'cmeloche':'Christian'}
 
