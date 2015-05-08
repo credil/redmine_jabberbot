00b6925726ba3c438dd29ed614c2c0c41b8e8f69
Julien Lamarche <julien.lamarche@gmail.com>
2015-05-08 15:47:28 -0400

    added monthly objectives algorithm

---
+++ monthlyObjectives.py
@@ -0,0 +1,7 @@
+In monthlyOjbectives config file, have:
+- hoursPerMonth: dict with entry per year/month
+- hoursPerWeekPerProject: dict with entry per user, with a sub-dict per project of hours / week
+
+If the month is not set, assume all weekdays minus one day
+
+Announce  total hours per to date for the month per project / objective end of month calculated as per 
