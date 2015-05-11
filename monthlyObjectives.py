149927f1a13d082827440e722188e7786420a267
Julien Lamarche <julien.lamarche@gmail.com>
2015-05-11 16:23:09 -0400

    added more algorithm to monthly objectives

--- monthlyObjectives.py
+++ monthlyObjectives.py
@@ -1,7 +1,13 @@
 In monthlyOjbectives config file, have:
-- hoursPerMonth: dict with entry per year/month
 - hoursPerWeekPerProject: dict with entry per user, with a sub-dict per project of hours / week
 
-If the month is not set, assume all weekdays minus one day
+- daysOff: List of standard days off for all users,
+- perUserExceptions: dict with a pair of users : list of exceptions (-not working : + working)
+
+
+How to get hours per user per project? (H)
+How to calculate total expected work days for a given user? (E)
+How to calculate work days so far? (W)
+How to calclulate pacing ? (P = T / W * E)
 
 Announce  total hours per to date for the month per project / objective end of month calculated as per 
