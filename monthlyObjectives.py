bf1594845e8e6f2822d806aeccac6d2ea233f2a4
Julien Lamarche <julien.lamarche@gmail.com>
2015-07-09 15:18:30 -0400

    redid monthly objectives with a more simple per user algorithm and just presenting the deltas on end of month estimate

--- monthlyObjectives.py
+++ monthlyObjectives.py
@@ -1,13 +1,67 @@
-In monthlyOjbectives config file, have:
-- hoursPerWeekPerProject: dict with entry per user, with a sub-dict per project of hours / week
+In config file:
+- Number of hours  per week per project
 
-- daysOff: List of standard days off for all users,
-- perUserExceptions: dict with a pair of users : list of exceptions (-not working : + working)
+For specified user, get:
+
+Number of hours per project spent last 28 days
+
+select p.identifier, sum(te.hours) as s 
+from time_entries te, projects p 
+where user_id = 419 
+  and spent_on between now() - INTERVAL '28 days' and now()
+  and te.project_id = p.id 
+group by p.identifier 
+order by s desc;
+
+Number of hours per project spent since beginning of month
+
+select p.identifier, sum(te.hours) as s
+from time_entries te, projects p 
+where user_id = 419 
+  and spent_on between date_trunc('month', current_date) and now() 
+  and te.project_id = p.id 
+group by p.identifier 
+order by s desc;
 
 
-How to get hours per user per project? (H)
-How to calculate total expected work days for a given user? (E)
-How to calculate work days so far? (W)
-How to calclulate pacing ? (P = T / W * E)
 
-Announce  total hours per to date for the month per project / objective end of month calculated as per 
+Calculate:
+Fraction of the work day: 
+(now - beginHour) / (endHour - beginHour)  
+Begin and endHour will be 9 to 5 to start
+
+
+4 week total = 27 + fraction
+
+Number of weekdays in month:
+################################################################################
+import datetime
+
+def buisnessDays(untilDay = 32)
+	now = datetime.datetime.now()
+	#holidays = [datetime.date(2013, 8, 14)] # you can add more here
+	holidays = [] # you can add more here
+	businessdays = 0
+	for i in range(1, untilDay):
+	    try:
+	        thisdate = datetime.date(now.year, now.month, i)
+	    except(ValueError):
+	        break
+	    if thisdate.weekday() < 5 and thisdate not in holidays: # Monday == 0, Sunday == 6 
+	        businessdays += 1
+	
+	print businessdays
+################################################################################
+
+For each project: 
+	Calulate expected for 28 days:
+	expected28[user][prj] = hoursPerWeekPerPrj[user][prj]*(27+fraction)/5
+	
+	Calulate expected from beginning of month:
+	expectedMonth[user][prj] = hoursPerWeekPerPrj[user][prj]*(buisnessDays+fraction)/5
+
+	deltaTodayForMonth[user][prj] = hoursPerWeekPerPrj[user][prj] - hoursWorkedForMonth[user][prj]
+
+
+
+Print the deltas
