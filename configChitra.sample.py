c13bd9923d7838232d67310f6b373b86af26b4b2
Julien Lamarche <jlam@credil.org>
2016-03-01 17:18:59 -0500

    added configChitra.sample.py

---
+++ configChitra.sample.py
@@ -0,0 +1,7 @@
+from datetime import datetime, date , timedelta
+settings = {
+	'jlam': {
+		'py3c': 	{'hoursPerWeek': 3*8, 'since': date(2015, 05, 01), 'ignore': ('thisConsultingProject')},
+	}
+}
+
